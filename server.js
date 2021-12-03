530bd698fd6e07d5cf9421e299ef0c70efbd12a0
Srezzx <srieshimportant@gmail.com>
2021-12-03 09:43:59 +0530

    added some stuff in server

--- server.js
+++ server.js
@@ -1,8 +1,36 @@
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://vish1504:<password>@cluster0.wotzs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
+var express = require("express");
+var methodOverride = require("method-override");
+var bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const expressLayouts = require("express-ejs-layouts");
+
+var app = express();
+app.use(methodOverride("_method"));
+app.set("view engine", "ejs");
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public/images"));
+const { MongoClient } = require("mongodb");
+
+const uri =
+  "mongodb+srv://vish1504:<password>@cluster0.wotzs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
   const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
   client.close();
+});
+
+app.get("/", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.listen(process.env.PORT || 3000, process.env.ID, function (req, res) {
+  console.log("Server has started  at PORT 3000");
 });
--- views/app.js
+++ views/app.js
@@ -2,42 +2,41 @@ var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-var User = require("./info")
+var User = require("./info");
 const internal = require("stream");
 
 mongoose.Promise = global.Promise;
 
-
-mongoose.connect("mongodb+srv://vish1504:abcd@cluster0.wotzs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority").then(() => {
-    console.log('Connected to DB!');
-}).catch(err => {
-    console.log('Error', err.message);
-});
-
+mongoose
+  .connect(
+    "mongodb+srv://vish1504:abcd@cluster0.wotzs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to DB!");
+  })
+  .catch((err) => {
+    console.log("Error", err.message);
+  });
 
 var app = express();
-app.set('views','views')
-app.set('view engine','ejs')
-app.use(express.urlencoded({extended:false}))
+app.set("views", "views");
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/',(req,res) =>{
-    res.render('plan1.ejs');
-})
-
+app.get("/", (req, res) => {
+  res.render("plan1.ejs");
+});
 
 app.post("/info", (req, res) => {
   var myData = new Info(req.body);
-  myData.save()
-    .then(item => {
+  myData
+    .save()
+    .then((item) => {
       res.send("item saved to database");
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(400).send("unable to save to database");
     });
 });
 
-
 app.listen(3000);
-
-
-
